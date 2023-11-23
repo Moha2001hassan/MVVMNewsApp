bbed19dfbd7a7179223b107d61bf37c187901473
Mohamed Hassan <mohahassanna933@gmail.com>
2023-11-23 23:07:11 +0200

    Retrofit Setup

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "39accd08f91c4e4d895020d8e267ce37"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
